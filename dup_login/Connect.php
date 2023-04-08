56ec89b68d774b34e42a79303d1a8477c47dea01
Fakhri Hamdan <fakhri.hf90@gmail.com>
2023-04-09 06:10:38 +0700

    cobacoba

---
+++ dup_login/Connect.php
@@ -0,0 +1,43 @@
+<?php 
+
+// MEMBUAT KONEKSI DATABASE
+class myConnect {
+    private $hostname = 'localhost';
+    private $username = 'root';
+    private $password = '';
+    private $db = "log_multiuser";
+
+    public function __construct() {
+        $this->db = new mysqli($this->hostname, $this->username, $this->password, $this->db);
+
+        //check koneksi
+        if ($this->db->connect_error) {
+            die("Koneksi gagal: " . $this->db->connect_error);
+        }  
+    }
+
+    public function getConnect() {
+        return $this->db;
+    }
+}
+//END KONEKSI DATABASE
+
+
+class Users {
+    private $email;
+    private $password;
+
+    function set_loginData($email, $password) {
+        $this->email = $email;
+        $this->password = $password;
+    }
+    
+    function getEmail() {
+        return $this->email;
+    }
+
+    function getPassword() {
+        return $this->password;
+    }
+}
+?>
