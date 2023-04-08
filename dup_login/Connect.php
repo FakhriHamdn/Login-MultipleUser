59bd778c426736b3242e4343c6e8491459ed117e
Fakhri Hamdan <fakhri.hf90@gmail.com>
2023-04-08 20:49:27 +0700

    melakukan beberapa perubahan pertama

---
+++ dup_login/Connect.php
@@ -0,0 +1,30 @@
+<?php 
+
+// MEMBUAT KONEKSI DATABASE
+class myConnect {
+    private $hostname = 'localhost';
+    private $username = 'root';
+    private $password = '';
+    private $db = "log_multiuser";
+
+    public function __construct() {
+        $this->db = new mysqli($this->hostname, $this->username, $this->password, $this->db);
+
+        //check koneksi
+        if ($this->db->connect_error) {
+            die("Koneksi gagal: " . $this->db->connect_error);
+        }  
+    }
+
+    public function getConnect() {
+        return $this->db;
+    }
+}
+//END KONEKSI DATABASE
+
+
+
+
+
+
+?>
---
+++ dup_login/index.php
@@ -0,0 +1,51 @@
+<?php
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://fonts.googleapis.com/css2?family=Poppins&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="stylee.css">
+    <title>Login Page</title>
+</head>
+
+<body>
+    <div class="container">
+        <section class="wrapper">
+            <div class="title">
+                <h2 class="page-title">Login Page</h2>
+                <h3 class="welcome-message">Welcome to website attendance</h3>
+                <span class="login-text">login</span>
+                <!-- buat munculin notif -->
+            </div>
+            <div>
+                <form action="login.php" method="POST" class="form-login">
+                    <div class="input-kolom">
+
+                        <div class="input-deployed">
+                            <label for="nip">Input Deployed Id</label>
+                            <input type="number" placeholder="Deployed id" name="nip" class="input-login" required>
+                        </div>
+                        <div class="input-pass">
+                            <label for="password">Input Password</label>
+                            <input type="password" placeholder="******" name="password" class="input-login" required>
+                        </div>
+                        <button type="submit" class="btn" name="login">Submit</button>
+                        <div class="register-container">
+                        <p class="acc-text">Don't have an account?
+                            <span class="register-text"><a href="register.php">Register</a></span>
+                        </p>
+                        </div>
+                    </div>
+                    <br>
+                </form>
+            </div>
+        </section>
+    </div>
+
+</body>
+</html>
