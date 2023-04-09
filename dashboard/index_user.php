bc4a401c7fbab3626e6c26da9670cad2d27ee23a
Fakhri Hamdan <fakhri.hf90@gmail.com>
2023-04-09 13:53:51 +0700

    modif dikit

--- dashboard/index_user.php
+++ dashboard/index_user.php
@@ -15,33 +15,11 @@ if(isset($_POST['logout'])) {
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="../style.css">
-    <title>Document</title>
+    <title>INDEX USER</title>
 </head>
 <body>
-    <!-- <?php 
-//    if($_SESSION['role'] == 'admin'){
-//     echo "<h2>Selamat datang</h2>" . $_SESSION['fullname'];
-//     echo "<h4>Status :</h4>" . $_SESSION['role'];
-//     echo "<a href='database.php'>Database</a>";
-// } else {
-//     echo "<h2>Selamat datang</h2>" . $_SESSION['fullname'];
-//     echo "<h4>Status :</h4>" . $_SESSION['role'];
-// }
-
-    ?> -->
-
-
     <h2>Selamat Datang <?php echo $_SESSION['fullname'];?></h2>
     <p>status : <?php echo $_SESSION['role'];?> </p>
-    <h3>Ini table admin ceritanya</h3>
-    <p>males buat bikin sendiri aja</p>
-    <table>
-        <tr>
-            <th>Id</th>
-        </tr>
-    </table>
-    
-    
     <form action="" method="POST">
     <button name="logout" type="submit">logout</button>
     </form>
