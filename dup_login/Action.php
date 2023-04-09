d0da90b408d74333ea63ce90711aeabed14520e4
Fakhri Hamdan <fakhri.hf90@gmail.com>
2023-04-09 10:31:12 +0700

    bingung mau ngasih text apaan, banyak syntax yang ditambahin intinya

--- dup_login/Action.php
+++ dup_login/Action.php
@@ -1,18 +1,43 @@
 <?php 
+//kita akan panggil class objek dari class user
+require 'connection.php';
 require 'Connect.php';
-$user = new Users();
 
-$user->set_loginData($_POST['email'], $_POST['password']);
-$email = $user->getEmail();
-$password = $user->getPassword();
-$sql = "SELECT * FROM users WHERE email = '$email' AND password = '$password'";
+$user = new myUsers(); //new berarti user akan terbentuk sebagai objek, dan termasuk instansiasi
 
-$conn = new myConnect();
-$connection = $conn->getConnect();
-$result = $connection->query($sql);
+if(isset($_POST['submit'])) { //submit dpt dari name button, jadi ketik submit diclick maka lakukan..
 
-var_dump($result);
+    //validasi password yang diinputkan user
+    if(strlen($_POST['password']) <= 5) {
+        header("location: index.php?message=invalid data");
+    } else {
+    //manggil data dari user, kita meng set data loginnya dari inputan form ke bagian belakang, bagian belakangnya itu class myUsers objeknya
+    $user->set_loginData($_POST['email'], $_POST['password']);
 
+    $emailId = $user->getEmail();
+    $pass = $user->getPassword();
+    $sql = "SELECT * FROM users WHERE email = '$emailId' AND password = '$pass'"; //pake kutip karena bukan int
+    
+    $conn = new myConnect();
+    $connection = $conn->getConnect();
+    $result = $connection->query($sql);
+
+    //untuk ngecek datanya ada atau ngk
+    if($result->num_rows > 0) {
+        echo "Data Cocok";
+    } else {
+        echo "data tidak cocok";
+    }
+
+}
+}
+
+
+
+
+// $user->set_loginData($_POST['email'], $_POST['password']);
+// $email = $user->getEmail();
+// $password = $user->getPassword();
 
 
 ?>
--- dup_login/Connect.php
+++ dup_login/Connect.php
@@ -1,5 +1,37 @@
 <?php 
 
+//myUsers ini merepresentasikan adanya data, ada email fullname, password yang ada didatabase.
+//nanti kita bisa isi data ini lewat depan, dengan function yang namanya set_logindata
+//nanti kita ambil lagi datanya dengan metode get
+class myUsers {
+    private $email;
+    private $password;
+
+    //user bisa set login data, setter getter
+    function set_loginData($email, $password) {
+        $this->email = $email; // variable penampung diprivate class, akan diisi oleh $this $email. $this email akan memanggil yang ada diclass user, menaggil yang ada diclass lalu isi debgab email diparameter, klo parameter pasti ada yang ngirim, yang ngirimnua dari index.php
+        $this->password = $password;
+    }
+
+    //BAGIAN GET NYA, untuk mengambil data dari class email dan password, karena diclassya sudah berisi daro set_loginData
+    //untuk connect ke database
+    function getEmail() {
+        return $this->email;
+    }
+    function getPassword() {
+        return $this->password;
+    }
+    //END GET
+
+    function set_profilData() {
+
+    }
+
+
+}
+
+
+
 // MEMBUAT KONEKSI DATABASE
 class myConnect {
     private $hostname = 'localhost';
@@ -23,21 +55,5 @@ class myConnect {
 //END KONEKSI DATABASE
 
 
-class Users {
-    private $email;
-    private $password;
 
-    function set_loginData($email, $password) {
-        $this->email = $email;
-        $this->password = $password;
-    }
-    
-    function getEmail() {
-        return $this->email;
-    }
-
-    function getPassword() {
-        return $this->password;
-    }
-}
 ?>
